051acb46acca4260e9d489798698d71b777f67d7
trytocry5 <davidseamuspkc@gmail.com>
2022-03-12 16:22:22 +0300

    firstCommit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Mobile_Apps_Development_Company_Logo_-_Codeexceptional.png/1024px-Mobile_Apps_Development_Company_Logo_-_Codeexceptional.png' />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,27 @@
+
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://html5css.ru/css/img_lights.jpg' />
+            </div>
+            <div>
+                ava + desc
+                <img src='https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg' />
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
